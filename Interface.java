3352012720424dc43a71310cd4ad2fcb79d01122
whizkidzm <plecopower2@gmail.com>
2024-10-03 10:10:26 -0500

    java files

---
+++ Interface.java
@@ -0,0 +1,10 @@
+package phase2;
+
+public interface Interface
+{
+	public void add(Point p);
+	public void delete(Point p);
+	public boolean search(Point p);
+	public String toString();
+	
+}
---
+++ Negotiate.java
@@ -0,0 +1,8 @@
+package phase2;
+
+//change to negotiator
+public interface Negotiate
+{
+	public Point respondToBid(Point p);
+	public Point startPoint();
+}
