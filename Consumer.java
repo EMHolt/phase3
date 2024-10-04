abd1d959bab97746a3001fb4d0fd5f625f0f73e4
whizkidzm <plecopower2@gmail.com>
2024-10-04 10:06:13 -0500

    Update Consumer.java

--- Consumer.java
+++ Consumer.java
@@ -2,40 +2,40 @@ package phase2;
 
 public class Consumer implements Negotiator {
 
-	private ProducerCurve pCurve;
+	private ConsumerCurve cCurve;
 	
 	public Consumer()
 	{
-		pCurve = new ProducerCurve(10, new Point(10, 1.0), 1, 1);
+		cCurve = new ConsumerCurve(10, new Point(10, 1.0), 1, 1);
 	}
 	
 	public Point startPoint()
 	{
-		return pCurve.getPoint(0);
+		return cCurve.getPoint(0);
 	}
 	
 	public Point respondToBid(Point p)
 	{ 
 		Point respondPoint = new Point();
-		if (pCurve.contains(p) == true)
+		if (cCurve.contains(p) == true)
 		{
 			respondPoint = p;
 		}
-		else if (pCurve.contains(p) == false)
+		else if (cCurve.contains(p) == false)
 		{
-			for(int i = 0; i < pCurve.getSize(); i++)
+			for(int i = 0; i < cCurve.getSize(); i++)
 			{
-				if(p.getQuantity() == pCurve.getPoint(i).getQuantity() && p.getPrice() > pCurve.getPoint(i).getPrice())
+				if(p.getQuantity() == cCurve.getPoint(i).getQuantity() && p.getPrice() > cCurve.getPoint(i).getPrice())
 				{
 					respondPoint = p;
 				}
 			}
-			for (int i = 0; i < pCurve.getSize(); i++)
+			for (int i = 0; i < cCurve.getSize(); i++)
 			{
-				if(p.getQuantity() == pCurve.getPoint(i).getQuantity() && p.getPrice() < pCurve.getPoint(i).getPrice())
+				if(p.getQuantity() == cCurve.getPoint(i).getQuantity() && p.getPrice() < cCurve.getPoint(i).getPrice())
 				{
 					respondPoint.setQuantity(p.getQuantity() + 1);
-					respondPoint = pCurve.getPoint(i+1);
+					respondPoint = cCurve.getPoint(i+1);
 				}
 			}
 		}
